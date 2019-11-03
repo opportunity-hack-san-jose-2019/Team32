4e61a472ba4dd3a16f0d4a46a5155158df2192ae
pratikb25 <pratik.bhandarkar@sjsu.edu>
2019-11-03 11:36:51 -0800

    making it better

--- backend/sewa-appserver.py
+++ backend/sewa-appserver.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from flask_mysqldb import MySQL
 from flask_cors import CORS, cross_origin
+import json
 
 app = Flask(__name__)
 
@@ -12,6 +13,13 @@ app.config['MYSQL_PORT'] = 3306
 
 mysql = MySQL(app)
 CORS(app, support_credentials=True)
+
+def format(cur, rv):
+    json_data=[]
+    row_headers=[x[0] for x in cur.description]
+    for result in rv:
+        json_data.append(dict(zip(row_headers,result)))
+    return json_data
 
 @cross_origin(supports_credentials=True)
 @app.route('/donate', methods = ['GET', 'POST'])
@@ -25,16 +33,75 @@ def donate():
         cur.execute("INSERT INTO donors(name, contact, email) VALUES(%s, %s, %s)", (name, contact, email))
         items = data['items']
         for item in items:
-        	cur.execute("INSERT INTO inventory(description, quantity, category) VALUES(%s, %s, %s)", (item['description'], item['quantity'], item['category']))
+            cur.execute("INSERT INTO inventory(description, quantity, category) VALUES(%s, %s, %s)", (item['description'], item['quantity'], item['category']))
         mysql.connection.commit()
         rv = cur.fetchall()
         return str(rv)
+
+
+@cross_origin(supports_credentials=True)
+@app.route('/allcases', methods = ['GET'])
+def handle_cases():
+    if request.method == 'GET':
+        cur = mysql.connection.cursor()
+        cur.execute("select * from cases")
+        mysql.connection.commit()
+        rv = cur.fetchall()
+        json_data = format(cur, rv)
+        cur.close()
+        return {"cases" : json_data}
+
+@cross_origin(supports_credentials=True)
+@app.route('/unassignedcases', methods = ['GET'])
+def get_unassigned_cases():
+    cur = mysql.connection.cursor()
+    cur.execute("select * from cases where caseid not in (select caseid from assignments)")
+    mysql.connection.commit()
+    rv = cur.fetchall()
+    json_data = format(cur, rv)
+    cur.close()
+    return {"cases" : json_data}
+
+@cross_origin(supports_credentials=True)
+@app.route('/volunteers', methods = ['GET'])
+def get_volunteers():
+    cur = mysql.connection.cursor()
+    cur.execute("select * from users where role = 'volunteer'")
+    mysql.connection.commit()
+    rv = cur.fetchall()
+    json_data = format(cur, rv)
+    cur.close()
+    return {"volunteers" : json_data}
+
+@cross_origin(supports_credentials=True)
+@app.route('/validatecases', methods = ['POST'])
+def validate_cases():
+    if request.method == 'POST':
+        cur = mysql.connection.cursor()
+        data = request.get_json()
+        for key, val in data.items():
+            cur.execute("UPDATE cases SET priority=%s WHERE caseid=%s", val, key)
+            mysql.connection.commit()
+            return
+        
+@cross_origin(supports_credentials=True)
+@app.route('/login', methods = ['POST'])
+def login():
+    data = request.get_json()
+    print(data['username'])
+    cur = mysql.connection.cursor()
+    cur.execute("select pwd from users where username like %s", [data['username']])
+    mysql.connection.commit()
+    rv = cur.fetchone()[0]
+    if rv == data['password']:
+        return "1"
+    return "0"
 
 @cross_origin(supports_credentials=True)
 @app.route('/register', methods = ['GET', 'POST'])
 def register():
     if request.method == 'POST':
-    	cur = mysql.connection.cursor()
+        cur = mysql.connection.cursor()
         data = request.get_json()
         firstname = data['firstname']
         lastname = data['lastname']
